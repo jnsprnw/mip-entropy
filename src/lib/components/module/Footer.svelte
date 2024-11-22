a9bf1e067b6ddd3b0b2503dcd58d55c66c45f1b1
Jonas Parnow <6gqrbj@posteo.org>
2024-11-22 15:23:03 +0100

    Button styling

--- src/lib/components/module/Footer.svelte
+++ src/lib/components/module/Footer.svelte
@@ -5,16 +5,19 @@
 	const story = getStoryState();
 
 	interface Props {
-		prevPage: Function;
-		nextPage: Function;
+		prevPage: () => void;
+		nextPage: () => void;
 	}
 	const { prevPage, nextPage }: Props = $props();
 </script>
 
-<footer class="bg-bg-dark p-5">
-	<menu class="grid grid-cols-3 justify-items-center items-center gap-x-4">
-		<Button onclick={prevPage} disabled={story.page === 0} label="Previous" />
-		<span>{story.pageNumber} / {story.totalPages}</span>
-		<Button onclick={nextPage} disabled={story.page === story.maxPage} label="Next" />
-	</menu>
-</footer>
+<menu class="bg-bg-dark px-2 py-2 grid grid-cols-3 justify-items-center items-center gap-x-4">
+	<Button isButtonPrevious={true} onclick={prevPage} disabled={story.page === 0} label="Previous" />
+	<span>{story.pageNumber} / {story.totalPages}</span>
+	<Button
+		isButtonNext={true}
+		onclick={nextPage}
+		disabled={story.page === story.maxPage}
+		label="Next"
+	/>
+</menu>
--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -1,20 +1,40 @@
 <script lang="ts">
+	import Right from '$lib/icons/Right.svelte';
+	import Left from '$lib/icons/Left.svelte';
+
 	interface Props {
-		onclick: Function;
+		onclick: () => void;
 		label: string;
 		disabled?: boolean;
 		isActive?: boolean;
+		isButtonNext?: boolean;
+		isButtonPrevious?: boolean;
 	}
-	const { onclick, label, disabled = false, isActive = false }: Props = $props();
+	const {
+		onclick,
+		label,
+		disabled = false,
+		isActive = false,
+		isButtonNext = false,
+		isButtonPrevious = false
+	}: Props = $props();
 </script>
 
 <button
 	{disabled}
 	aria-pressed={isActive}
-	class="text-white disabled:text-neutral-200 disabled:cursor-not-allowed h-9 px-4 py-2 shadow-sm shadow-black/[0.04] hover:bg-black/80 inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring/70 focus-visible:ring-offset-2 disabled:pointer-events-none"
+	class="gap-x-2 text-white disabled:text-neutral-200 disabled:cursor-not-allowed h-9 px-6 py-2 shadow-sm shadow-black/[0.04] hover:bg-black/80 inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring/70 focus-visible:ring-offset-2 disabled:pointer-events-none"
 	class:bg-amber-800={isActive}
 	class:bg-black={!isActive}
 	class:disabled:bg-neutral-500={!isActive}
 	class:disabled:bg-amber-700={isActive}
-	{onclick}>{label}</button
+	{onclick}
 >
+	{#if isButtonPrevious}
+		<Left />
+	{/if}
+	{label}
+	{#if isButtonNext}
+		<Right />
+	{/if}
+</button>
