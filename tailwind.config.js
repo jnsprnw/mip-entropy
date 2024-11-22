6a4a40d032c363288960742cf5a8d8c4f43ffa24
Jonas Parnow <6gqrbj@posteo.org>
2024-11-22 14:39:47 +0100

    Basic colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				bg: {
+					bright: '#D5EDF5',
+					dark: '#CBE8EF'
+				},
+				accent: {
+					dark: '#005CDE',
+					light: '#00B2E2'
+				},
+				grid: {
+					outer: '#4E63A9',
+					inner: '#C3EBFA'
+				},
+				bar: {
+					line: '#B2E2EA',
+					bar: '#E5322E'
+				}
+			}
+		}
 	},
 	plugins: []
 };
