d35550b1da5b0b0265afd6e97d9bbfec0efe6498
Jonas Parnow <6gqrbj@posteo.org>
2024-11-22 17:57:14 +0100

    Links added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
-<script lang="ts">
-	import Story from '$lib/components/story/Story.svelte';
-</script>
-
-<Story />
+<div class="flex items-center justify-center w-screen h-screen">
+	<div class="bg-white flex flex-col gap-y-3 font-semibold p-5">
+		<a href="/1-entropy">What Is Entropy?</a>
+		<a href="/2-uncertainty">Entropy as Uncertainty</a>
+		<a href="/3-exploiting">Exploiting Entropy’s Rise</a>
+		<a href="/4-subjective">Entropy Is Subjective</a>
+		<a href="/5-knowledge">Knowledge Is Power</a>
+	</div>
+</div>
