d699432977b5e9f1550d893e105bc05d96c4463b
Jonas Parnow <6gqrbj@posteo.org>
2024-11-22 15:27:30 +0100

    Clean up

--- src/lib/components/module/Footer.svelte
+++ src/lib/components/module/Footer.svelte
@@ -2,22 +2,18 @@
 	import { getStoryState } from '$lib/components/story/story-state.svelte';
 	import Button from '$lib/components/ui/Button.svelte';
 
-	const story = getStoryState();
-
 	interface Props {
 		prevPage: () => void;
 		nextPage: () => void;
 	}
 	const { prevPage, nextPage }: Props = $props();
+
+	const { is_first_page, is_last_page, current_page_number, total_pages } =
+		$derived(getStoryState());
 </script>
 
 <menu class="bg-bg-dark px-2 py-2 grid grid-cols-3 justify-items-center items-center gap-x-4">
-	<Button
-		isButtonPrevious={true}
-		onclick={prevPage}
-		disabled={story.is_first_page}
-		label="Previous"
-	/>
-	<span>{story.current_page_number} / {story.total_pages}</span>
-	<Button isButtonNext={true} onclick={nextPage} disabled={story.is_last_page} label="Next" />
+	<Button isButtonPrevious={true} onclick={prevPage} disabled={is_first_page} label="Previous" />
+	<span>{current_page_number} / {total_pages}</span>
+	<Button isButtonNext={true} onclick={nextPage} disabled={is_last_page} label="Next" />
 </menu>
