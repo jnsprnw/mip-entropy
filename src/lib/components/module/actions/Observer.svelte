d952236ba90fd3fbf305e6ca790ff123c9a4f6cd
Jonas Parnow <6gqrbj@posteo.org>
2024-11-25 22:44:01 +0100

    Clean up

--- src/lib/components/module/actions/Observer.svelte
+++ src/lib/components/module/actions/Observer.svelte
@@ -2,21 +2,10 @@
 	import { getGridState } from '$lib/components/grid/grid-state.svelte';
 	import Button from '$lib/components/ui/Button.svelte';
 	import { getObserverDetail } from '$lib/components/grid/utils-order';
-	import { OBSERVER_ALICE, OBSERVER_BOB } from '$config';
+	import { OBSERVER_ALICE } from '$config';
 	const gridState = getGridState();
 	const { grid } = $derived(gridState);
-	const {
-		setObserver,
-		allow_observer_switch,
-		hasObserver,
-		observer,
-		sort,
-		can_sort,
-		entropy,
-		is_sorting
-	} = $derived(grid);
-
-	// const label = $derived(observer === OBSERVER_ALICE ? 'Bob' : 'Alice');
+	const { setObserver, allow_observer_switch, hasObserver, observer } = $derived(grid);
 
 	const button_area = $derived(observer === OBSERVER_ALICE ? 'col-start-1' : 'col-start-4');
 
