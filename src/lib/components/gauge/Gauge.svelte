d1c109f37d86733fa7fd3ee52b9056c76ea03e2f
Jonas Parnow <6gqrbj@posteo.org>
2024-11-25 17:07:00 +0100

    Aria added

--- src/lib/components/gauge/Gauge.svelte
+++ src/lib/components/gauge/Gauge.svelte
@@ -4,6 +4,8 @@
 	import { cubicOut } from 'svelte/easing';
 	import { PADDING_GRID } from '$config';
 
+	const gauge_id = crypto.randomUUID();
+
 	interface Props {
 		value: number;
 		label?: string;
@@ -25,13 +27,21 @@
 <div
 	class="grid grid-rows-[1fr_auto] gap-y-2 w-full h-full justify-items-center"
 	style="padding-top: {PADDING_GRID.top}px; box-sizing: border-box;"
+	role="meter"
+	aria-valuenow={value}
+	aria-valuemin="0"
+	aria-valuemax="100"
+	aria-labelledby={gauge_id}
 >
+	>
 	<span
 		class="bg-entropy-bar h-full w-[5px] justify-self-center place-self-end col-start-1 col-end-2 row-start-1"
+		aria-hidden="true"
 	></span>
 	<span
 		style="height: {scale($progress)}%;"
 		class="bg-highlight w-2/5 justify-self-center place-self-end col-start-1 col-end-2 row-start-1"
+		aria-hidden="true"
 	></span>
-	<span class="text-highlight font-semibold text-center text-xs/none">{label}</span>
+	<span id={gauge_id} class="text-highlight font-semibold text-center text-xs/none">{label}</span>
 </div>
