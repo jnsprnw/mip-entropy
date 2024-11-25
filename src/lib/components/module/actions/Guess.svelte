fa38281227cb99bcf6e5872ff49faeb61e2f2e50
Jonas Parnow <6gqrbj@posteo.org>
2024-11-25 22:46:33 +0100

    Display text when all particles are found
    #17

--- src/lib/components/module/actions/Guess.svelte
+++ src/lib/components/module/actions/Guess.svelte
@@ -13,7 +13,13 @@
 	<div class="col-start-1 col-span-5 row-start-2 grid grid-cols-2 text-center items-center gap-x-4">
 		<div class="flex flex-col transition-opacity" class:opacity-50={!canGuess}>
 			<span class="text-xs">Guesses: {count_guess}</span>
-			<span class="text-xs">{count_found} / {GUESS_PARTICLE_COUNT} particles found</span>
+			<span class="text-xs">
+				{#if count_found < GUESS_PARTICLE_COUNT}
+					{count_found} / {GUESS_PARTICLE_COUNT} particles found
+				{:else}
+					Good job
+				{/if}
+			</span>
 		</div>
 		<Button
 			label="Reset grid"
