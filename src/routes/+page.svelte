d1457b689bfeeccebeb88f83e8a7e28ea73acb5c
Jonas Parnow <6gqrbj@posteo.org>
2024-12-09 12:21:08 +0100

    Clean up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,5 @@
 		<a href="/2-uncertainty">Entropy as Uncertainty</a>
 		<a href="/3-exploiting">Exploiting Entropy’s Rise</a>
 		<a href="/4-subjective">Entropy Is Subjective</a>
-		<a href="/5-knowledge">Knowledge Is Power</a>
 	</div>
 </div>
