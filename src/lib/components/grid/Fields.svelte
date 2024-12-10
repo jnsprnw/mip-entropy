695edb3cabbcaddf20eeb3b9ee66649ce8acc6ff
Jonas Parnow <6gqrbj@posteo.org>
2024-12-10 16:43:18 +0100

    Fix blur in Safari

--- src/lib/components/grid/Fields.svelte
+++ src/lib/components/grid/Fields.svelte
@@ -6,6 +6,10 @@
 	const { grid } = $derived(gridState);
 </script>
 
+<filter id="blur" x="-50%" y="-50%" width="200%" height="200%">
+	<feGaussianBlur in="SourceGraphic" stdDeviation="3" />
+</filter>
+
 <g class="fields">
 	{#each grid.fields as opt, i}
 		<Field position={opt?.index ?? i} {opt} />
--- src/lib/components/grid/shapes/Circle.svelte
+++ src/lib/components/grid/shapes/Circle.svelte
@@ -9,4 +9,10 @@
 	const { color, cx = 0, cy = 0, r = 8, isBlur = false }: Props = $props();
 </script>
 
-<circle {cx} {cy} {r} class="fill transition-colors {color}" class:blur-sm={isBlur} />
+<circle
+	{cx}
+	{cy}
+	{r}
+	class="fill transition-colors {color}"
+	filter={isBlur ? 'url(#blur)' : undefined}
+/>
