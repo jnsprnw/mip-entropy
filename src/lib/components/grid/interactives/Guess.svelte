2a5b14b6b8c4d2f3d011104c76fbd81b2fa70d45
Jonas Parnow <6gqrbj@posteo.org>
2024-12-10 17:01:46 +0100

    Clean up

--- src/lib/components/grid/interactives/Guess.svelte
+++ src/lib/components/grid/interactives/Guess.svelte
@@ -2,24 +2,26 @@
 	import { getGridState } from '$grid/grid-state.svelte';
 	const gridState = getGridState();
 	const { grid } = $derived(gridState);
+	const { mode, canGuess, guesses, guess } = $derived(grid);
 	import { MODE_GUESS, GRID_SIZE } from '$config';
 	import { range } from 'd3-array';
 	import { fade } from 'svelte/transition';
 </script>
 
-{#if grid.mode === MODE_GUESS}
+{#if mode === MODE_GUESS}
 	<div
 		in:fade={{ duration: 500 }}
 		class="grid justify-items-stretch content-stretch gap-[1px] border-2 border-grid-outer rounded-[5px] -m-px overflow-hidden"
 		style="grid-template-columns: repeat({GRID_SIZE}, 1fr); grid-template-rows: repeat({GRID_SIZE}, 1fr); height: calc(100% + 2px);"
 	>
 		{#each range(GRID_SIZE * GRID_SIZE) as position}
+			{@const is_guess = guesses[position]}
 			<button
-				class="bg-primary-dark outline outline-1 outline-grid-inner transition-[colors_opacity] focus:outline-none focus:bg-accent-dark hover:bg-primary-light"
-				disabled={!grid.canGuess || grid.guesses[position]}
-				class:bg-transparent={grid.guesses[position]}
-				class:pointer-events-none={grid.guesses[position]}
-				onclick={() => grid.guess(position)}
+				class="bg-primary-dark outline outline-1 outline-grid-inner transition-[colors_opacity] focus:outline-none focus:bg-primary-light hover:bg-primary-light"
+				disabled={!canGuess || is_guess}
+				class:bg-transparent={is_guess}
+				class:pointer-events-none={is_guess}
+				onclick={() => guess(position)}
 				aria-label="Click to guess position {position}"
 			></button>
 		{/each}
